a010fbdd62ca18abd2b90f9d0ce8ffcf92bd0215
Stas Elovsky <stas.elvosky@gmail.com>
2021-06-17 00:27:31 +0300

    COMMON: initialize project use cases

--- use-cases/src/commonMain/kotlin/com/helicoptera/togerio/user/ProjectManager.kt
+++ use-cases/src/commonMain/kotlin/com/helicoptera/togerio/user/ProjectManager.kt
@@ -1,4 +1,17 @@
 package com.helicoptera.togerio.user
 
+import com.helicoptera.data.model.project.ProjectMember
+
 interface ProjectManager {
+
+    fun createProject(name: String, code: String, description: String)
+
+    fun changeProject(projectId: Int, name: String, code: String, description: String)
+
+    fun deleteProject(projectIfd: Int)
+
+    fun inviteToProject(projectId: Int, userId: Int)
+
+    fun getProjectMembers(projectId: Int) : List<ProjectMember>
+
 }
