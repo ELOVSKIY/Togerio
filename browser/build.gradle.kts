85fa0cb699a04521fdfcea027cd5b25e3484a189
Stas Elovsky <stas.elvosky@gmail.com>
2021-07-13 00:53:13 +0300

    CLIENT: update gitignore

--- browser/build.gradle.kts
+++ browser/build.gradle.kts
@@ -1,6 +1,23 @@
 plugins {
     kotlin("js")
 }
+val kotlin_version: String by project
+val wrapper_version: String by project
+val react_version: String by project
+val kotlin_styled_version: String by project
+val router_dom_version: String by project
+val redux_version: String by project
+val react_redux_version: String by project
+val ring_ui_version: String by project
+
+println(kotlin_version)
+println(wrapper_version)
+println(react_version)
+println(kotlin_styled_version)
+println(router_dom_version)
+println(redux_version)
+println(react_redux_version)
+println(ring_ui_version)
 
 group = "com.helicoptera"
 version = "1.0-SNAPSHOT"
@@ -8,17 +25,19 @@ version = "1.0-SNAPSHOT"
 repositories {
     jcenter()
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlin/p/kotlin/kotlin-js-wrappers") }
+    maven { url = uri("https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers") }
 }
 
 dependencies {
     testImplementation(kotlin("test-js"))
     implementation(project(":model"))
-    implementation("org.jetbrains:kotlin-react:16.13.1-pre.113-kotlin-1.4.0")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.113-kotlin-1.4.0")
-    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.113-kotlin-1.4.0")
-    implementation("org.jetbrains:kotlin-react-router-dom:5.1.2-pre.113-kotlin-1.4.0")
-    implementation("org.jetbrains:kotlin-redux:4.0.0-pre.113-kotlin-1.4.0")
-    implementation("org.jetbrains:kotlin-react-redux:5.0.7-pre.113-kotlin-1.4.0")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:$react_version-pre.$wrapper_version-kotlin-$kotlin_version")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:$react_version-pre.$wrapper_version-kotlin-$kotlin_version")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:$kotlin_styled_version-pre.$wrapper_version-kotlin-$kotlin_version")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:$router_dom_version-pre.$wrapper_version-kotlin-$kotlin_version")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-redux:$redux_version-pre.$wrapper_version-kotlin-$kotlin_version")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-redux:$react_redux_version-pre.$wrapper_version-kotlin-$kotlin_version")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-ring-ui:$ring_ui_version-pre.$wrapper_version-kotlin-$kotlin_version")
 }
 
 kotlin {
