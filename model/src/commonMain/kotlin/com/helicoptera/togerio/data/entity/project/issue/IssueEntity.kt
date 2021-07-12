16cbc319ef6fd1cf4273f520f87bd5d212d2e5aa
Stas Elovsky <stas.elvosky@gmail.com>
2021-07-13 00:55:32 +0300

    COMMON: update models

--- model/src/commonMain/kotlin/com/helicoptera/togerio/data/entity/project/issue/IssueEntity.kt
+++ model/src/commonMain/kotlin/com/helicoptera/togerio/data/entity/project/issue/IssueEntity.kt
@@ -2,19 +2,13 @@ package com.helicoptera.togerio.data.entity.project.issue
 
 data class IssueEntity(
     val id: Int,
-    val projectId: Int,
-    val code: Int,
-    val number: Int,
+
     val title: String,
     val description: String? = null,
+    val number: Int,
+    val code: String,
+    val projectId: Int,
     val priorityId: Int,
     val typeId: Int,
     val stateId: Int,
-    val fixVersionsToIssueId: Int,
-    val affectVersionsToIssueId: Int,
-    val priorityEntity: PriorityEntity? = null,
-    val type: TypeEntity? = null,
-    val state: StateEntity? = null,
-    val fixVersions: List<VersionEntity> = listOf(),
-    val affectVersions: List<VersionEntity> = listOf()
 )
--- model/src/commonMain/kotlin/com/helicoptera/togerio/data/entity/project/issue/StateEntity.kt
+++ model/src/commonMain/kotlin/com/helicoptera/togerio/data/entity/project/issue/StateEntity.kt
@@ -2,6 +2,7 @@ package com.helicoptera.togerio.data.entity.project.issue
 
 data class StateEntity (
     val id: Int,
+    val projectId: Int,
     val title: String,
     val description: String,
     val complete: Boolean,
