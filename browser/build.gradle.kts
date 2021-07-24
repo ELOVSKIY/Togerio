cfe6fe206a057b4efa41cbd9bb389836de1923a3
Stas Elovsky <stas.elvosky@gmail.com>
2021-07-25 02:22:25 +0300

    CLIENTL: update versions add ctor to browser module

--- browser/build.gradle.kts
+++ browser/build.gradle.kts
@@ -9,6 +9,9 @@ val router_dom_version: String by project
 val redux_version: String by project
 val react_redux_version: String by project
 val ring_ui_version: String by project
+val ktor_version: String by project
+val coroutines_version: String by project
+val logback_version: String by project
 
 println(kotlin_version)
 println(wrapper_version)
@@ -18,12 +21,14 @@ println(router_dom_version)
 println(redux_version)
 println(react_redux_version)
 println(ring_ui_version)
+println(ktor_version)
 
 group = "com.helicoptera"
 version = "1.0-SNAPSHOT"
 
 repositories {
     jcenter()
+    mavenCentral()
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlin/p/kotlin/kotlin-js-wrappers") }
     maven { url = uri("https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers") }
 }
@@ -31,6 +36,16 @@ repositories {
 dependencies {
     testImplementation(kotlin("test-js"))
     implementation(project(":model"))
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-js:$ktor_version")
+    implementation("io.ktor:ktor-client-serialization:$ktor_version")
+    implementation("io.ktor:ktor-client-auth:$ktor_version")
+    implementation("io.ktor:ktor-client-logging:$ktor_version")
+
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
+
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react:$react_version-pre.$wrapper_version-kotlin-$kotlin_version")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:$react_version-pre.$wrapper_version-kotlin-$kotlin_version")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:$kotlin_styled_version-pre.$wrapper_version-kotlin-$kotlin_version")
