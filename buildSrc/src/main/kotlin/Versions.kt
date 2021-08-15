cfb99105f1760657f65492dee0f416a7f5384445
Stas Elovsky <stas.elvosky@gmail.com>
2021-08-16 01:06:33 +0300

    COMMON: add buildSrc for dependency managment

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,27 @@
+object Versions {
+    //    #Dependencies versions
+    val ktor_version = 1.6.1
+    val kotlin_version = 1.5.20
+    val logback_version = 1.2.3
+    val h2_version = 1.4.199
+    val exposed_version = 0.30.1
+    val checkstyle_version = 8.15
+    val jacoco_version = 0.8.2
+    val coroutines_version = 1.5.1
+
+    //    #Browser dependencies
+    val wrapper_version = 215
+    val react_version = 17.0.2
+    val kotlin_styled_version = 5.3.0
+    val router_dom_version = 5.2.0
+    val redux_version = 4.1.0
+    val react_redux_version = 7.2.4
+    val ring_ui_version = 4.0.21
+
+    object Android {
+        val compileSdkVersion = 29
+        val minSdkVersion = 24
+        val versionCode = 1
+        val versionName = "1.0.0"
+    }
+}
